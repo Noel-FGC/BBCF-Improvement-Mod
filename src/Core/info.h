05bd21c0c1575bfbdbda15cc773c27012ddbc44a
Noel <noel-fgc@proton.me>
2025-07-25 21:45:04 -0500

    update info.h

--- src/Core/info.h
+++ src/Core/info.h
@@ -1,6 +1,6 @@
 #pragma once
 #define MOD_VERSION	"v3.099"
-#define MOD_VERSION_NUM	MOD_VERSION " BO BLEIS edition"
+#define MOD_VERSION_NUM	MOD_VERSION " I'm So Tired edition"
 #define MOD_WINDOW_TITLE "BBCF IM"
 
 #define MOD_FORCE_DISABLE_UPDATE_CHECK 0
@@ -8,6 +8,6 @@
 // Links
 #define MOD_LINK_DISCORD L"https://discord.gg/j2mCX9s"
 #define MOD_LINK_FORUM L"https://steamcommunity.com/app/586140/discussions/0/3195868146163014015/"
-#define MOD_LINK_GITHUB L"https://github.com/libreofficecalc/BBCF-Improvement-Mod/releases"
-#define MOD_LINK_API_GITHUB_RELEASE L"https://api.github.com/repos/libreofficecalc/BBCF-Improvement-Mod/releases"
+#define MOD_LINK_GITHUB L"https://github.com/Noel-FGC/BBCF-Improvement-Mod/releases"
+#define MOD_LINK_API_GITHUB_RELEASE L"https://api.github.com/repos/Noel-FGC/BBCF-Improvement-Mod/releases"
 #define REPLAY_DB_FRONTEND L"http://50.118.225.175:2000/"
